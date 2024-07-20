ab5a18bc6aca5aac5082c88ed10721544decf700
Sahil7570 <sahilmehraa0@gmail.com>
2024-07-20 15:47:24 +0530

    Update leetcode-Q2.cpp

--- leetcode-Q2.cpp
+++ leetcode-Q2.cpp
@@ -9,16 +9,9 @@ int main()
     int n;
     cout<<"Enter any Digits : ";
     cin>>n;
-    int max=pow(2,31)-1;
-    int min=pow(-2,31)-1;
-    
     int ans=0;
  while(n!=0)
  {
-    if(ans<min || ans<max)
-    {
-        cout<<0;
-    }
     int ld=n%10;
     ans=(ans*10)+ld;
     n=n/10;
